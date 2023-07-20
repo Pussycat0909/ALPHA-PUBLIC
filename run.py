f9b47703ecfb5a3136028aba09cd82e962905c38
ALPHA REX <139839305+alpharexx@users.noreply.github.com>
2023-07-20 21:36:38 +0545

    Update run.py

--- run.py
+++ run.py
@@ -4,9 +4,9 @@ print(" CHECKING FOR UPDATES")
 os.system('git pull -q')
 if bit == '32bit' :
 	print(" 32 BIT DETECTED")
-	import PUBLIC32
-	PUBLIC32.login()
+	import new32
+	new32.login()
 elif bit == '64bit' :
 	print(" 64 BIT DETECTED ")
-	import PUBLIC64
-	PUBLIC64.login()
+	import new64
+	new64.login()
