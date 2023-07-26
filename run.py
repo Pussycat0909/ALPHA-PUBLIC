98fa8c28409a922faee7c14f403b59f1fa742586
ALPHA REX <139839305+alpharexx@users.noreply.github.com>
2023-07-26 12:40:01 +0545

    Add files via upload

--- run.py
+++ run.py
@@ -1,10 +1,21 @@
-import os, platform
+
+import os,platform
+
+try:
+    import openai , requests , bs4, pyotp, mechanize, future
+except ModuleNotFoundError:
+    print("\n INSTALLING MISSING MODULES ")
+    os.system('pip install pyotp -q && pip install mechanize -q && pip install future -q && pip install openai -q && pip install requests --quiet && pip install bs4 --quiet')
+    exit("\n MODULE HAS BEEN INSTALLED RERUN BY python run.py ")
+
 bit = platform.architecture()[0]
-print(" CHECKING FOR UPDATES")
+print(" CHECKING FOR UPDATES ")
 os.system('git pull -q')
-if bit == '32bit' :
-	print(" 32 BIT DETECTED")
-	os.system('python .p')
-elif bit == '64bit' :
-	print(" 64 BIT DETECTED ")
-	os.system('python .p')
+if bit == '32bit':
+ print(" 32 BIT DETECTED")
+ import NEW32
+ NEW32.login()
+elif bit == '64bit':
+ print(" 64 BIT DETECTED")
+ import NEW64
+ NEW64.login()
